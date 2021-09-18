2533dfdce3477264dbbab28bb68b8aac4dc7f037
spdevi <spneuro01@gmail.com>
2021-09-18 13:56:35 +0900

    Initialize repo for OCR feature

---
+++ src/main/kotlin/com/tech/phreeoni/ocrizator/controller/IOCRController.kt
@@ -0,0 +1,5 @@
+package com.tech.phreeoni.ocrizator.controller
+
+interface IOCRController {
+
+}
---
+++ src/main/kotlin/com/tech/phreeoni/ocrizator/controller/OCRController.kt
@@ -0,0 +1,10 @@
+package com.tech.phreeoni.ocrizator.controller
+
+import net.sourceforge.tess4j.*;
+import java.io.File;
+
+class OCRController : IOCRController{
+    fun processImageFile(imageFile: File) {
+
+    }
+}
