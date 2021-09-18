86e244c143ee94e56d9d5ca8cadf1a24d697968d
spdevi <spneuro01@gmail.com>
2021-09-18 19:08:28 +0900

    Add OCR sample prototyp

--- src/main/kotlin/com/tech/phreeoni/ocrizator/OcrizatorApplication.kt
+++ src/main/kotlin/com/tech/phreeoni/ocrizator/OcrizatorApplication.kt
@@ -1,11 +1,16 @@
 package com.tech.phreeoni.ocrizator
 
+import com.tech.phreeoni.ocrizator.controller.OCRController
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.File
 
 @SpringBootApplication
 class OcrizatorApplication
 
 fun main(args: Array<String>) {
-	runApplication<OcrizatorApplication>(*args)
+    runApplication<OcrizatorApplication>(*args)
+    System.out.println("Starting image ocr...")
+    var ocrController = OCRController()
+	ocrController.processImageFile()
 }
--- src/main/kotlin/com/tech/phreeoni/ocrizator/controller/OCRController.kt
+++ src/main/kotlin/com/tech/phreeoni/ocrizator/controller/OCRController.kt
@@ -4,7 +4,17 @@ import net.sourceforge.tess4j.*;
 import java.io.File;
 
 class OCRController : IOCRController{
-    fun processImageFile(imageFile: File) {
+    fun processImageFile() {
 
+        try {
+            var imageFile = File("/home/spdevi/Pictures/conan_ocr_sample_1.png")
+            val tesseract = Tesseract();
+            tesseract.setDatapath("/usr/share/tessdata")
+            tesseract.setLanguage("jpn")
+            var result = tesseract.doOCR(imageFile);
+            System.out.println(result)
+        } catch(e : Exception) {
+            System.out.println(e)
+        }
     }
 }
